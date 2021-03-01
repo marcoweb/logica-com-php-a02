940eae13c1662b5a6b22b6c9a0b05cda2fc5b6c8
marcoweb <marcocarvalho.web@gmail.com>
2021-03-01 14:29:39 +0000

    Criado Index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Primeiro Exemplo</title>
+    </head>
+    <body>
+        <h1>
+        <?php echo 'Olá Web!!!'; ?>
+        </h1>
+        <p>
+            <?= 'Olá sem echo' ?>
+        </p>
+    </body>
+</html>
